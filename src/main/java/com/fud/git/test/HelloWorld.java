900ecf1768dbed427ebf96fdc2ff48ba9b7888b3
bailaoke <bailaoke@wacai.com>
2019-04-12 15:37:56 +0800

    Initial commit

---
+++ src/main/java/com/fud/git/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.fud.git.test;
+
+/**
+ * @author bailaoke
+ * @date 2019-04-12 15:24
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
