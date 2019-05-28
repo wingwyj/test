cc417b46befbc6901e747e8d301b594beb05e684
bailaoke <bailaoke@wacai.com>
2019-05-28 14:57:15 +0800

    add brance

--- src/main/java/com/fud/git/test/HelloWorld.java
+++ src/main/java/com/fud/git/test/HelloWorld.java
@@ -8,6 +8,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        //brance
+        /**
+         *  add bailoake brance
+         */
     }
 }
